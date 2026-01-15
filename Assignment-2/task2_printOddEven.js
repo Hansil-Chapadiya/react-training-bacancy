321b7781ebd073b2bbd584db8f6ecebfbd79c481
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2026-01-15 15:08:27 +0530

    print odd even and slicing array

--- Assignment-2/task2_printOddEven.js
+++ Assignment-2/task2_printOddEven.js
@@ -1,10 +1,23 @@
 
-for (let i = 0; i < 20; i++) {
-    if (i % 2 == 0) {
+// for (let i = 0; i < 20; i++) {
+//     if (i % 2 == 0) {
 
-        console.log(`${i} - > Eevn`);
-    }
-    else{
-        console.log(i + " -> Odd")
-    }
-}
+//         console.log(`${i} - > Eevn`);
+//     }
+//     else{
+//         console.log(i + " -> Odd")
+//     }
+// }
+
+a = [1,2,3,4];
+
+a = a.slice(2);
+
+// const b=a.map(ele=>ele*2);
+
+// console.log(b);
+
+
+a.forEach(element => {
+    console.log(element);
+});
