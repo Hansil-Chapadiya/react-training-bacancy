94b8726782d47827f9181a0a1181d55542d5053f
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2026-01-27 17:22:56 +0530

    Done using object(Key/Value) pair

--- Assignment-2/task3_switch_case_role.js
+++ Assignment-2/task3_switch_case_role.js
@@ -1,16 +1,26 @@
-role = process.argv.slice(2);
+const role = process.argv.slice(2);
 
-switch (role[0])
-{
-    case 'ADMIN':
-        console.log("ADMIN -> Full Access"); break;
-    
-    case 'USER':
-        console.log("USER -> Limited Access"); break;
-    
-    case 'MANAGER':
-        console.log("MANAGER -> Moderate Access"); break;
-    
-    default:
-        console.log("default -> Invalid Access");
+console.log(role);
+const role_ = {
+
+    'ADMIN' : "ADMIN -> FULL ACCESS",
+    'USER' : "USER -> Limited Access",
+    'MANAGER' : "MANAGER -> Moderate Access",
 }
+console.log(role_[role[0]?role[0].toUpperCase():""]?role_[role[0].toUpperCase()] :"Invalid Access"); 
+
+
+// switch (role[0])
+// {
+//     case 'ADMIN':
+//         console.log("ADMIN -> Full Access"); break;
+    
+//     case 'USER':
+//         console.log("USER -> Limited Access"); break;
+    
+//     case 'MANAGER':
+//         console.log("MANAGER -> Moderate Access"); break;
+    
+//     default:
+//         console.log("default -> Invalid Access");
+// }
