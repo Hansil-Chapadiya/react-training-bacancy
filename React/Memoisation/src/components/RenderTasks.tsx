801316b88e24099466d0a72cc5e53977e034e8bd
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2026-03-25 16:36:06 +0530

    Basic Css added

--- React/Memoisation/src/components/RenderTasks.tsx
+++ React/Memoisation/src/components/RenderTasks.tsx
@@ -12,22 +12,39 @@ const RenderTasks = () => {
             : tasksList.tasks
     }, [tasksList, showCompleted]);
     return (
-        <div>
+        <div className="taskContainer">
             Show Completed Task :
             <input type="checkbox" onChange={toggleCompletedView} />
             {filteredTasks.map((task) => (
                 <div key={task.id} className="taskItem">
-                    <input type="checkbox" checked={task.completed} onChange={() => handleToggle(task.id)} />
-                    <h3 style={{
-                        textDecoration: task.completed ? "line-through" : "none"
-                    }}>{task.title}</h3>
-                    <p style={{
-                        textDecoration: task.completed ? "line-through" : "none"
-                    }}>{task.description}</p>
-                    <button onClick={() => { handleDelete(task.id) }}>Delete</button>
+
+                    <input
+                        type="checkbox"
+                        checked={task.completed}
+                        onChange={() => handleToggle(task.id)}
+                    />
+
+                    <div className="taskContent">
+                        <h3 className={task.completed ? "completed" : ""}>
+                            {task.title}
+                        </h3>
+                        <p className={task.completed ? "completed" : ""}>
+                            {task.description}
+                        </p>
+                    </div>
+
+                    <button
+                        className="deleteBtn"
+                        onClick={() => handleDelete(task.id)}
+                    >
+                        Delete
+                    </button>
                 </div>
             ))}
-            <button type="button" onClick={handleClear}>Clear</button>
+
+            <button className="clearBtn" onClick={handleClear}>
+                Clear All
+            </button>
         </div>
     )
 }
